cc52f5536a16b86fc287fa8403220ccf6c2674ab
Randy-Nunez <randynunez468@gmail.com>
2025-09-30 13:01:50 -0400

    completed outfit picker function

--- index.js
+++ index.js
@@ -7,7 +7,19 @@ Below 40 → "Wear a heavy coat, scarf, and boots."
 */
 
 const outfitPicker = (temp) => {
-  // ✍️ Write your code here
+  if (temp >= 80) {
+    return "Wear shorts and a t-shirt.";
+  } else if (temp >= 60 && temp <= 79) {
+    return "Wear jeans and a light jacket.";
+  } else if (temp >= 40 && temp <= 59) {
+    return "Wear a sweater and pants.";
+  } else if (temp < 40) {
+    return "Wear a heavy coat, scarf, and boots.";
+  }
 };
 
 //Test your function below
+console.log(outfitPicker(93));
+console.log(outfitPicker(75));
+console.log(outfitPicker(47));
+console.log(outfitPicker(0));
